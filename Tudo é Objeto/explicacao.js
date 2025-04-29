99853c5db7b89c950fbdf083d628815db48b5953
Adriano <adriano.almeida09@outlook.com>
2025-04-28 22:47:34 -0300

    Tudo é Objeto em JavaScript

---
+++ Tudo é Objeto/explicacao.js
@@ -0,0 +1,9 @@
+/* Tudo é Objeto */
+// String, Números, Boolean, Objetos e mais, possuem propriedades e métodos. Por isso são objetos.
+var nome = "Adriano";
+
+nome.length; // 7
+nome.charAt(1); // 'd'
+nome.replace("no", "não"); // 'Adrianão'
+nome; // 'Adriano'
+// Uma string herda propriedades e métodos do construtor String()
