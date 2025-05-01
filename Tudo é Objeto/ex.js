4600960738a8724eaeb1dc8a97fc04cae5f8672b
Adriano <adriano.almeida09@outlook.com>
2025-04-30 22:07:24 -0300

    Exercicio de métodos e propriedades

--- Tudo é Objeto/ex.js
+++ Tudo é Objeto/ex.js
@@ -1,4 +1,16 @@
 // Nomeie 3 Propriedades ou métodos de strings
+var teste = "Adriano";
+teste.toString();
+teste.toUpperCase();
+teste.charAt(0);
+
 // Nomeie 5 propriedades ou métodos de elementos do DOM.
+var nada = document.querySelector();
+nada = document.querySelectorAll();
+nada = document.getElementById();
+nada = document.getElementsByClassName();
+nada = document.getElementsByName();
+
 // Busque na web um objeto (método) capaz de interagir com o clipboard
 // clipboard é a parte do seu computador que lida com CTRL + C e CTRL + V
+Clipboar.writeText();
