01983c724dc8de96fb618d6d8eb703b481a7004a
Adriano <adriano.almeida09@outlook.com>
2025-05-01 13:58:32 -0300

    Exibindo a resolução do exercicio utilizando o forEach

--- Arrays e Loops/ex.js
+++ Arrays e Loops/ex.js
@@ -4,13 +4,15 @@ const copas = [1959, 1962, 1970, 1994, 2002];
 // Interaja com a array utilizando um loop, para mostrar
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
 
-for (let i = 0; i < copas.length; i++) {
-  console.log(`O Brasil ganhou a copa de: ${copas[i]}`);
-}
+/* Opção 1 de resolução */
+// for (let i = 0; i < copas.length; i++) {
+//   console.log(`O Brasil ganhou a copa de: ${copas[i]}`);
+// }
 
-// copas.forEach(function (ano) {
-//   console.log(`O Brasil ganhou a copa de ${ano}`);
-// });
+/* Opção 2 de resolução */
+copas.forEach(function (ano) {
+  console.log(`O Brasil ganhou a copa de ${ano}`);
+});
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
 var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
