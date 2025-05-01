093984290ffae8cac124410e2c67384311b204c5
Adriano <adriano.almeida09@outlook.com>
2025-04-30 22:15:57 -0300

    Finalizada a aula Tudo é Objeto em JavaScript

--- Tudo é Objeto/ex.js
+++ Tudo é Objeto/ex.js
@@ -1,15 +1,17 @@
 // Nomeie 3 Propriedades ou métodos de strings
+// Sempre que tem um método, método é uma função
 var teste = "Adriano";
 teste.toString();
 teste.toUpperCase();
 teste.charAt(0);
 
 // Nomeie 5 propriedades ou métodos de elementos do DOM.
-var nada = document.querySelector();
-nada = document.querySelectorAll();
-nada = document.getElementById();
-nada = document.getElementsByClassName();
-nada = document.getElementsByName();
+var nada = document.querySelector(".btn");
+nada.addEventListener();
+nada.appendChild();
+nada.id();
+// nada.innerHTML();
+// nada.outerHTML();
 
 // Busque na web um objeto (método) capaz de interagir com o clipboard
 // clipboard é a parte do seu computador que lida com CTRL + C e CTRL + V
