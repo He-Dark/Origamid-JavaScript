c4bdf5a78d6ae0d9db2f35c32606e7c3ab7b51c7
Adriano <adriano.almeida09@outlook.com>
2025-05-03 18:53:27 -0300

    Finalizando a Aula sobre Escopo de Função

--- Escopo de Funcao/explicacao.js
+++ Escopo de Funcao/explicacao.js
@@ -31,3 +31,60 @@ function mostrarCarro() {
 
 mostrarCarro(); //Meu carro é um Fusca
 console.log(carro);
+
+/* Escopo de Bloco */
+// Variáveis criadas com var, vazam o bloco. Por isso com a introdução do ES6 a melhor forma de declarar uma variável é utilizando const e let, pois estas respeitam o escopo de bloco
+if (true) {
+  var carro = "Fusca";
+  console.log(carro);
+}
+console.log(carro);
+
+/* Const e Let no lugar de Var */
+// A partir de agora vamos utilizar apenas const e let para declarar variáveis.
+if (true) {
+  const carro = "Fusca";
+  console.log(carro);
+}
+console.log(carro); // erro, carro is not defined
+
+/* {} Cria um bloco */
+// Chaves {} criam um escopo de bloco, não confundir com a criação de objetos = {}
+{
+  var carro = "Fusca";
+  const ano = 2018;
+}
+console.log(carro);
+console.log(ano); // erro ano is not defined
+
+/* For Loop */
+// Ao utilizar var dentro de um for loop, que é um bloco, o valor da variável utilizada irá vazar e existir fora do loop.
+for (var i = 0; i < 10; i++) {
+  console.log(`Número ${i}`);
+}
+console.log(i); // 10
+
+/* Const */
+// Mantém o escopo no bloco, impede a redeclaração e impede a modificação do valor da variável, evitando bugs no código.
+const mes = "Dezembro";
+mes = "Janeiro"; // erro, tentou modificar o valor
+// const semana; // erro, declarou sem valor
+
+const data = {
+  dia: 28,
+  mes: "Dezembro",
+  ano: 2018,
+};
+
+data.dia = 29; // Funciona
+data = "Janeiro"; // erro
+
+/* Let */
+// Mantém o escopo do bloco, impede a redeclaração, mas permite a modificação do valor da variável.
+let ano1;
+ano1 = 2018;
+ano1++;
+console.log(ano1); // 2019
+
+// let ano1 = 2020; // erro, redeclarou a variável.
+// Geralmente vamos utilizar o const
--- Escopo de Funcao/script.js
+++ Escopo de Funcao/script.js
@@ -5,4 +5,35 @@ function mostrarCarro() {
 }
 
 mostrarCarro(); // Fusca no console
-console.log(carro);
+// console.log(carro);
+
+if (true) {
+  let mes = "Dezembro";
+  console.log(mes);
+}
+// console.log(mes);
+
+{
+  var carro2 = "Fusca2";
+  const ano = 2018;
+}
+console.log(carro2);
+// console.log(ano); // erro ano is not defined
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(`Número ${i}`);
+// }
+// console.log(i); // 10
+
+// console.log(i * 10);
+const semana = "Sexta";
+// semana = "Quinta";
+console.log(semana);
+
+const data = {
+  ano: 2018,
+  mes: "Dezembro",
+};
+
+data.ano = 2019;
+data.dia = 25;
